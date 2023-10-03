2ee943ad3fa1429a9a6ddbbd8300705e9d062253
Nastya2228 <128375621+Nastya2228@users.noreply.github.com>
2023-10-03 22:01:39 +0300

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,113 @@
+
+
+# #
+# a= int(input())
+# b= a + 1
+# c = a + 2
+# print(a,b,c ,sep = "\n")
+# #
+
+# q = int(input())
+# w = int(input())
+# e = int(input())
+# print(q + w + e)
+#
+# a = int(input())
+# volume = a ** 3
+# square = 6 * a ** 2
+# print("Объем",volume)
+# print("Площадь полной поверхности ",square)
+
+
+# a = int(input())
+# b = int(input())
+# result = 3 * (a + b) ** 3 + 275 * b ** 2 - 127 * a - 41
+# print(result)
+
+# a = int(input())
+# previous = a - 1
+# next = a + 1
+# print(f"Следующее за числом {a} число:" ,next)
+# print(f"Для числа {a} предыдущее число: ",previous)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# result = (a + b + c + d)*3
+# print(result)
+
+
+# a = int(input())
+# d = int(input())
+# n = int(input())
+# result = a + ( n - 1) * d
+# print(result)
+
+# a = int(input())
+# print(a,2*a,3*a,4*a,5*a,sep="---")
+
+# n = int(input())
+# k = int(input())
+# print(k//n)
+# print(k%n)
+
+# n = int(input())
+# print((n//2) + n%2)
+
+# n = int(input())
+# b = n//60
+# c = n%60
+# print(f"{n} мин - это {b} час {c} минут")
+
+
+
+# i = int(input())
+# ai = str(ki)
+#
+# num1 = int(ai[0]) + int(ai[1]) + int(ai[2])
+# num2 = int(ai[0]) * int(ai[1]) * int(ai[2])
+# print("Сумма цифр =", num1)
+# print("Произведение цифр =" , num2)
+
+
+# a= int(input())
+# print('     ~     '*a)
+# print('    - -    '*a)
+# print('   (о о)   '*a)
+# print(' /   ^   / '*a)
+# print('/(   _   )/'*a)
+# print('  ^^   ^^  '*a)
+
+
+# a = int(input())
+# b = (a // 10) % 10
+# print(b)
+
+
+
+# a = int(input())
+# hours = a % (60 * 24) // 60
+# minutes = a % 60
+# print(hours, minutes)
+
+
+# x = int(input())
+# print(1 - x)
+
+
+#x = int(input())
+#print((x//2+1)*2)
+
+
+#y = int(input())
+#t = int(input())
+#x = y * t
+#n = x // 109
+#print(-(109 * n - x))
+
+
+#q = float(input())
+#a = float(input())
+#b = float(input())
+#print(int(1 + (q - b - 1) / (a - b)))
